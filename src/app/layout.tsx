1177940b20f3df79e24ee90c53e98718ad6ef4c4
roeintheglasses <hrkjangir@gmail.com>
2024-01-22 18:16:41 +0530

    Added Support for NextUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
 
-import { TRPCReactProvider } from "~/trpc/react";
+import { Providers } from "./providers";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+// app/providers.tsx
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { TRPCReactProvider } from "~/trpc/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <TRPCReactProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </TRPCReactProvider>
+  );
+}
